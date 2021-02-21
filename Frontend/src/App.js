998bdccbee2835f31c5f3d61d2f3687705c520da
Astellarius <zak.lake0@gmail.com>
2021-02-20 17:20:08 -0800

    login form console.log App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usernameValue: '',
+            passwordValue: ''
+        };
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleUsernameChange(event) {
+        this.setState({usernameValue: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({passwordValue: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Username: ' + this.state.usernameValue + 'Password: ' + this.state.passwordValue);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+
+                <label>
+                    Username:
+                    <input 
+                        type="text" 
+                        value={this.state.value} 
+                        onChange={this.handleUsernameChange} 
+                    />
+                </label>
+
+                <label>
+                    Password:
+                    <input 
+                        type="text" 
+                        value={this.state.value} 
+                        onChange={this.handlePasswordChange} 
+                    />
+                </label>
+                
+                <input type="submit" value="Submit" />
+
+            </form>
+        );
+    }
 }
 
 export default App;
