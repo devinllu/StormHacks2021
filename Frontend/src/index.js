1549d11dba464337397885c0f6df4510ee228fc7
John <gtom_1984@hotmail.ca>
2021-02-21 03:31:02 -0800

    return package to normal

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,27 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Pokemon from './Pokemon';
 import Profile from './Components/ProfileData';
-import DocumentMeta from 'react-document-meta'
-
-
-const meta = {
-    title: 'meta',
-    meta: {
-      charset: 'utf-8',
-      name: "viewport",
-      content:"minimum-scale=1, initial-scale=1, width=device-width",
-    }
-  };
 
 ReactDOM.render(
     // <App />, 
     // <Pokemon />, 
-    <DocumentMeta {...meta} >
-        <Profile profileData={{
-            Name: "",
-            Contacts: [""],
-            Games: [""],
-        }}/>,
-    </DocumentMeta>,
+    <Profile profileData={{
+        Name: "",
+        Contacts: [""],
+        Games: [""],
+    }}/>,
     document.getElementById('root')
 );
