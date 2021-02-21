a5c64e9181f11e99d9bd617080b917430bb3fb45
John <gtom_1984@hotmail.ca>
2021-02-21 00:18:53 -0800

    Fix most errors

--- Frontend/src/Components/ProfileData.js
+++ Frontend/src/Components/ProfileData.js
@@ -8,14 +8,13 @@ function Profile({profileData}){
         Contacts: profileData.Contacts.slice(),
         Games: profileData.Games.slice(),
     })
-    console.log(profileData);
-
+    
     const applyProfileChanges = (profileData) => {
         setProfile({
             Name: profileData.Name,
             Contacts: profileData.Contacts.slice(),
             Games: profileData.Games.slice(),
-        })
+        });
     }
 
     return (
@@ -34,39 +33,40 @@ function Profile({profileData}){
                 <li key={index}>{game}</li>
             ))}
             </ul>
-            <EditProfileForm profileData={profileData} applyChanges={applyProfileChanges}/>
+            <EditProfileForm profileData={profileData} applyChanges={applyProfileChanges} />
         </div>
         
     );
 }
 
-function EditProfileForm({profileData}, {applyChanges}){
+function EditProfileForm(props){
     
     const [profile, setProfile] = React.useState({
-        Name: profileData.Name,
-        Contacts: profileData.Contacts.slice(),
-        Games: profileData.Games.slice(),
+        Name: props.profileData.Name,
+        Contacts: props.profileData.Contacts.slice(),
+        Games: props.profileData.Games.slice(),
     });
-    console.log(profileData.Contacts);
+    
 
     const handleSubmit = () => {
-        applyChanges(profileData);
-    }
+        props.applyChanges(profile);
+    };
 
     return (
         <div>
             <form>
             <label>Name</label>
-            <input type="text" onSubmit={e => {
+            <input type="text" onChange={e => {
                 setProfile({
                     Name: e.target.value,
                 })
                 console.log(profile);
             }}></input>
             </form>
-            {/*<FormListComponent
+            <button onClick={handleSubmit}>Apply Profile Changes</button>
+            <FormListComponent
                 entryList={profile.Contacts.slice()}
-                entry={"ere"}
+                entry={""}
                 entryName={"Contact"}
                 applyChanges={
                     (newEntries) => {
@@ -75,8 +75,7 @@ function EditProfileForm({profileData}, {applyChanges}){
                         })
                     }
                 }
-            /> */}
-            <button onClick={()=>handleSubmit()}></button>
+            />
         </div>
         
     )
@@ -84,34 +83,33 @@ function EditProfileForm({profileData}, {applyChanges}){
 
 // --- Custom Form List Component --- //
 // @TODO entry is a string not an object, so we might need to watch out for that
-function FormListComponent({entryList}, {entry}, {entryName}, {applyChanges}){
+function FormListComponent(props){
     
-    const [entries, setEntries] = React.useState(entryList);
-
-    console.log("entryList" + entryList);
-    console.log("entries" + entries);
-
+    const [entries, setEntries] = React.useState(props.entryList);
     const addNewEntry = () => {
         let newEntries = entries.slice();
-        newEntries.push(entry);
+        newEntries.push(props.entry);
         setEntries(newEntries);
     }
     return(
         <div>
-            <button onClick={addNewEntry()}>
-                {`Add new ${entryName}`}
+            <button onClick={addNewEntry}>
+                {`Add new ${props.entryName}`}
             </button>
-            {entries.map((entryItem, index) => (
-                <FormListElement 
-                    entry={entryItem}  
-                    deleteElement={() => {
-                        const newEntries = entries.splice(index, 1);
-                        setEntries(newEntries);
-                    }}
-                />
-            ))}
+            <ul>
+                {entries.map((item, index) => (
+                    <FormListElement 
+                        key={index}
+                        entry={item}  
+                        deleteElement={() => {
+                            const newEntries = entries.splice(index, 1);
+                            setEntries(newEntries);
+                        }}
+                    />
+                ))}
+            </ul>
             <button
-            onClick={() => applyChanges(entries)}>
+            onClick={() => props.applyChanges(entries)}>
                 {"Submit changes"}
             </button>
         </div>
@@ -119,13 +117,11 @@ function FormListComponent({entryList}, {entry}, {entryName}, {applyChanges}){
 }
 
 //--- Holds each entry with button --- ///
-function FormListElement({entry}, {deleteElement}){
+function FormListElement(props){
     return(
         <li>
-            <div>
-                <input type="text" value={entry}></input>
-                <button onClick={() => deleteElement()} value={"Delete"}></button>
-            </div>
+            <input type="text" value={props.entry}></input>
+            <button onClick={() => props.deleteElement()}>{"Delete"}</button>
         </li>
     )
 }
